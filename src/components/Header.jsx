7f5b562e941d9dc5a4086275834b8c92ca4aec67
frvanco <antoinevancompernolle@gmail.com>
2021-04-14 16:43:11 +0200

    optimise navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,29 +2,25 @@ import './Header.scss';
 import styled from 'styled-components';
 
 const ButtonWrapper = styled.section`
-  background-color: #f9f9f9;
-  padding: 20px;
+  background-color: grey;
+  padding: 30px;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
 `;
 
 const Button = styled.button`
   border-radius: 20px;
-  background-color: background-color: ${({ primary }) =>
-    primary ? '#E38B06' : '#000'};
-  color: color: ${({ primary }) => (primary ? '#000' : '#fff')};
   padding: 18px 30px;
-  font-size: 20px;
+  font-size: 30px;
   outline: none;
   cursor: pointer;
   border: none;
-  transition: transform .2s ease;
-  
-  &:hover{
-    background-color: ${({ primary }) => (primary ? '#fff' : '#E38B06')};  
-    transform: translateY(-.5rem) scale(1.02);
-    color: #000;
+  transition: transform 0.2s ease;
+  &:hover {
+    background-color: red;
+    transform: translateY(-0.5rem) scale(1.02);
+    color: black;
   }
 `;
 
@@ -33,16 +29,11 @@ export default function Header() {
     <header className="Header">
       <h1>SUPER HEROES</h1>
       <nav>
-        <ul>
-          <li>QUIZ</li>
-          <li>FIGHT</li>
-          <li>REQUEST</li>
-          <ButtonWrapper>
-            <Button> QUIZ </Button>
-            <Button> FIGHT </Button>
-            <Button> REQUEST </Button>
-          </ButtonWrapper>
-        </ul>
+        <ButtonWrapper>
+          <Button> QUIZ </Button>
+          <Button> FIGHT </Button>
+          <Button> REQUEST </Button>
+        </ButtonWrapper>
       </nav>
     </header>
   );
