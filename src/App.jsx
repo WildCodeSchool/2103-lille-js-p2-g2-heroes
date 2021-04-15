93ef31256c3c0139292ecf1261c31af54e2f92fb
mickaeldessin <mickaeldessin@outlook.fr>
2021-04-15 15:13:09 +0200

    add superheros api

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Main from './components/Main';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './reset.css';
+import SuperHerosApi from './components/SuperHerosApi';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Main />
       <Footer />
+      <SuperHerosApi />
     </div>
   );
 }
