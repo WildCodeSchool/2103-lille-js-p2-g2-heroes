b7cc2003371b673f36db345cdb0093ea76a28ac5
Maenhout <victor@maenhout.fr>
2021-04-19 23:57:09 +0200

    Ajout du background

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import styled from 'styled-components';
 import Home from './components/Home';
 import QuizPage from './components/QuizPage';
 import './reset.css';
 
+const SApp = styled.div`
+  background-image: url('/background-deus-ex-02.jpg');
+`;
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/quiz">
-            <QuizPage />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <SApp>
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/quiz">
+              <QuizPage />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </SApp>
   );
 }
 
