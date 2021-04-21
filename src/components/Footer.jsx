c943b6834c429c5fbbda6b50402cc9dbccb1bedb
Maenhout <victor@maenhout.fr>
2021-04-21 15:48:12 +0200

    Added footer style and copyright text.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,32 @@
 import styled from 'styled-components';
 
-const Sfooter = styled.footer`
-  background-color: black;
-  color: white;
+const SFooter = styled.footer`
+  background-color: #222222;
+  color: #555555;
+  font-size: 0.7em;
+  padding: 30px;
+  margin-top: 50px;
+  box-shadow: 0px -5px 10px black;
+  display: flex;
+  justify-content: center;
+`;
+
+const SCopyright = styled.div`
+  text-align: center;
+  line-height: 1.5em;
+  width: 30vw;
 `;
 
 export default function Footer() {
-  return <Sfooter>Footer</Sfooter>;
+  return (
+    <SFooter>
+      <SCopyright>
+        <p className="description">
+          The images used are not free of rights. For any request to remove an
+          image that you own, please contact the following email address:
+          ramdom@contact.com
+        </p>
+      </SCopyright>
+    </SFooter>
+  );
 }
