7f62630803b655e4ab345af11366da34f990fb9d
frvanco <antoinevancompernolle@gmail.com>
2021-04-21 16:46:52 +0200

    add question and in english

--- src/components/QuizPage.jsx
+++ src/components/QuizPage.jsx
@@ -12,175 +12,190 @@ export default function QuizPage() {
 
   const questions = [
     {
-      questionText: 'What is the first question ?',
+      questionText:
+        'If you were to become a superhero, what would your outfit be? ',
       answerOptions: [
         {
-          content: 'answer 1',
+          content: 'Make-up will do',
           stat: 'intelligence',
           value: 10,
         },
         {
-          content: 'answer 2',
+          content: 'Old pajamas and go!  ',
           stat: 'speed',
           value: 10,
         },
         {
-          content: 'answer 3',
+          content: 'I make myself an armor ',
           stat: 'strenght',
           value: 10,
         },
         {
-          content: 'answer 4',
+          content: 'Something that is scary',
           stat: 'power',
           value: 10,
         },
       ],
     },
     {
-      questionText: 'What is the second question ?',
+      questionText: 'What power would you like to have? ',
       answerOptions: [
         {
-          content: 'answer 1',
+          content: 'Be smart enough to build my outfit ',
           stat: 'intelligence',
           value: 10,
         },
         {
-          content: 'answer 2',
+          content: 'To be fast ',
           stat: 'speed',
           value: 10,
         },
         {
-          content: 'answer 3',
+          content: 'Be gifted in martial arts ',
           stat: 'strenght',
           value: 10,
         },
         {
-          content: 'answer 4',
+          content:
+            'Not having any powers, but just knowing how to create my own combat items ',
           stat: 'power',
           value: 10,
         },
       ],
     },
     {
-      questionText: 'What is the third question ?',
+      questionText: 'To get out of a sticky situation, what would you do? ',
       answerOptions: [
         {
-          content: 'answer 1',
+          content: 'You would divert attention with something surprising  ',
           stat: 'intelligence',
           value: 10,
         },
         {
-          content: 'answer 2',
+          content:
+            'You would escape and arrange so that no one could find you ',
           stat: 'speed',
           value: 10,
         },
         {
-          content: 'answer 3',
+          content: 'You would neutralize your enemies until the last ',
           stat: 'strenght',
           value: 10,
         },
         {
-          content: 'answer 4',
+          content: 'You would blow everything around you ',
           stat: 'power',
           value: 10,
         },
       ],
     },
     {
-      questionText: 'What is the fourth question ?',
+      questionText:
+        'You are at the end of the line, the enemy is far too powerful for you and the reinforcements are not coming ',
       answerOptions: [
         {
-          content: 'answer 1',
+          content:
+            'You wait for the endless monologue of the super villains about to win to stab him',
           stat: 'intelligence',
           value: 10,
         },
         {
-          content: 'answer 2',
+          content: 'You try to save time by hiding behind debris ',
           stat: 'speed',
           value: 10,
         },
         {
-          content: 'answer 3',
+          content: 'You keep on fighting even if it means dying ',
           stat: 'strenght',
           value: 10,
         },
         {
-          content: 'answer 4',
+          content:
+            'You use your last strength to ward off fatal blows and save time ',
           stat: 'power',
           value: 10,
         },
       ],
     },
     {
-      questionText: 'What is the fifth question ?',
+      questionText:
+        'Following intense fights between super heroes, planet Earth is no more than a deserted battlefield. What are you going to do now? ',
       answerOptions: [
         {
-          content: 'answer 1',
+          content: 'So much desolation we must repopulate the planet ',
           stat: 'intelligence',
           value: 10,
         },
         {
-          content: 'answer 2',
+          content: 'Im trying to find survivors ',
           stat: 'speed',
           value: 10,
         },
         {
-          content: 'answer 3',
+          content: 'Im sipping a refreshing drink now that Im finally alone! ',
           stat: 'strenght',
           value: 10,
         },
         {
-          content: 'answer 4',
+          content:
+            'Im leaving the blue planet because there is nothing more to do ',
           stat: 'power',
           value: 10,
         },
       ],
     },
     {
-      questionText: 'What is the sixth question ?',
+      questionText:
+        'At the bend of an alley you come across an individual dressed in a bat costume, he offers you a wedding ring, what do you do? ',
       answerOptions: [
         {
-          content: 'answer 1',
+          content: 'For you super heroes = super powers',
           stat: 'intelligence',
           value: 10,
         },
         {
-          content: 'answer 2',
+          content:
+            'Insulted, you push him in a dumpster and climb to the roof ',
           stat: 'speed',
           value: 10,
         },
         {
-          content: 'answer 3',
+          content:
+            'You join him without hesitation because you find that his armor inspires respect ',
           stat: 'strenght',
           value: 10,
         },
         {
-          content: 'answer 4',
+          content:
+            'You thank him kindly, but the Justice League is not really your thing',
           stat: 'power',
           value: 10,
         },
       ],
     },
     {
-      questionText: 'What is the seventh question ?',
+      questionText: 'In an epic fight, a little girl is caught in the flames ',
       answerOptions: [
         {
-          content: 'answer 1',
+          content: 'You run to save her and you use her as a human shield ',
           stat: 'intelligence',
           value: 10,
         },
         {
-          content: 'answer 2',
+          content:
+            'You ask the super villain to be fair and take a short break  ',
           stat: 'speed',
           value: 10,
         },
         {
-          content: 'answer 3',
+          content:
+            'You brave danger by burning yourself, but the girl is safe and sound ',
           stat: 'strenght',
           value: 10,
         },
         {
-          content: 'answer 4',
+          content:
+            'You violently throw a car on the ground in order to extinguish the flames and stun your opponent, at the risk of injuring the girl ',
           stat: 'power',
           value: 10,
         },
