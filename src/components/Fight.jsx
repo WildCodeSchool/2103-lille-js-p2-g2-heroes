a1c1c871e713e5608986cca0fa24200d4edca0b1
Georges FORTRY <gfortry@hotmail.com>
2021-04-28 14:35:52 +0200

    Conditional Rendering to fight page

--- src/components/Fight.jsx
+++ src/components/Fight.jsx
@@ -4,10 +4,19 @@ export default function Fight() {
   const location = useLocation();
 
   return (
-    <div>
-      <img src={location.state.myHero.image.url} alt="héros" />
-      <p>VS</p>
-      <img src={location.state.vsHero.image.url} alt="vshéros" />
-    </div>
+    <>
+      {!location.state && (
+        <div>
+          <p>Please do the quiz to get your own SuperHero!</p>
+        </div>
+      )}
+      {location.state && (
+        <div>
+          <img src={location.state.myHero.image.url} alt="héros" />
+          <p>VS</p>
+          <img src={location.state.vsHero.image.url} alt="vshéros" />
+        </div>
+      )}
+    </>
   );
 }
