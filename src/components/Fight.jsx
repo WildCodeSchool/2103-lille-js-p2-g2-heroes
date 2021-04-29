c828024bf7bc859f7bfe8981f087dc289c917cd2
Georges FORTRY <gfortry@hotmail.com>
2021-04-29 21:16:25 +0200

    fight button

--- src/components/Fight.jsx
+++ src/components/Fight.jsx
@@ -2,6 +2,24 @@ import { useLocation } from 'react-router-dom';
 
 export default function Fight() {
   const location = useLocation();
+  /* const myHero = {
+    int: parseInt(location.state.myHero.powerstats.intelligence, 10),
+    str: parseInt(location.state.myHero.powerstats.strength, 10),
+    spe: parseInt(location.state.myHero.powerstats.speed, 10),
+    dur: parseInt(location.state.myHero.powerstats.durability, 10),
+    pow: parseInt(location.state.myHero.powerstats.power, 10),
+    com: parseInt(location.state.myHero.powerstats.combat, 10),
+    img: location.state.myHero.image.url,
+  };
+  const vsHero = {
+    int: parseInt(location.state.vsHero.powerstats.intelligence, 10),
+    str: parseInt(location.state.vsHero.powerstats.strength, 10),
+    spe: parseInt(location.state.vsHero.powerstats.speed, 10),
+    dur: parseInt(location.state.vsHero.powerstats.durability, 10),
+    pow: parseInt(location.state.vsHero.powerstats.power, 10),
+    com: parseInt(location.state.vsHero.powerstats.combat, 10),
+    img: location.state.vsHero.image.url,
+  }; */
 
   return (
     <>
@@ -14,6 +32,7 @@ export default function Fight() {
           <img src={location.state.myHero.image.url} alt="héros" />
           <p>VS</p>
           <img src={location.state.vsHero.image.url} alt="vshéros" />
+          <button type="button">Ready for the fight... Come on !</button>
         </div>
       )}
     </>
